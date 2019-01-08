3451f2dc20d22329568fbc6bbdc9c11d1fea099f
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2019-01-08 14:11:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // add solution here
 
-function theBeatlesPlay(musicians, instruments){
-  var []
-  for (musicians = 0; musicians < 4; musicians++)
-  console.log("musicians"[0], "instruments"[0])
-  
-}
+ function theBeatlesPlay(musicians, instruments){
+   
+ }
 
